4c85d958178c181787f1d3e49d7e0160b344f788
Nikolay Red <40564813+nikolajred@users.noreply.github.com>
2021-10-26 19:37:59 +0300

    html practice

---
+++ app.js
@@ -0,0 +1,23 @@
+
+//массив картинок
+const imageArray = [
+"img/fruits.jpg",
+"img/fruits1.jpg",
+"img/fruits2.jpg",
+"img/fruits3.jpg",
+];
+
+window.number = 0;
+
+let imageCount = imageArray.length;
+
+let leftArrown = document.querySelector('.arrow_left');
+
+let rightArrow = document.querySelector('.arrow_right');
+
+let counter = document.querySelector('.counter');
+
+let image = document.querySelector('.image');
+
+
+
