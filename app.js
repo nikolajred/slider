01c0d2b287d5c0162200cd03044dc1a8898075c7
Nikolay <nikolajred@gmail.com>
2021-10-26 22:16:08 +0300

    second

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 
-//массив картинок
+document.addEventListener('DOMContentLoaded', () => {
+
 const imageArray = [
-"img/fruits.jpg",
-"img/fruits1.jpg",
-"img/fruits2.jpg",
-"img/fruits3.jpg",
-];
+{name: 'fruits',
+ id: 1,
+ img: 'img/fruits.jpg'},
+
+{name: 'fruits1',
+ id: 2,
+ img: 'img/fruits1.jpg'},
+
+{name: 'fruits2',
+ id: 3,
+ img: 'img/fruits2.jpg'},
+
+{name: 'fruits3',
+ id: 4,
+ img: 'img/fruits3.jpg'},
+
+{name: 'fruits4',
+ id: 5,
+ img: 'img/fruits4.jpg'}
+ ]
 
 window.number = 0;
+
+let id = Math.floor(Math.random() * 6);
+
+if(id >= 6 || id <=0){
+	id = 1;
+}
 
 let imageCount = imageArray.length;
 
@@ -17,7 +39,18 @@ let rightArrow = document.querySelector('.arrow_right');
 
 let counter = document.querySelector('.counter');
 
-let image = document.querySelector('.image');
+let slide = document.querySelector('.images');
 
+
+function createImage(){
+let pict = document.createElement('img');
+pict.setAttribute('src', `${imageArray[id].img}`);
+pict.setAttribute('id', `${id}` )
+slide.appendChild(pict);
+}
+
+createImage();
+
+});
 
 
